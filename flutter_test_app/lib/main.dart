9c77a6fdc763eb82330ca035cc472f5de4f45ade
Thányla Sales <thanylas@gmail.com>
2020-04-23 09:49:50 -0300

    Implementação de audios

--- flutter_test_app/lib/main.dart
+++ flutter_test_app/lib/main.dart
@@ -1,19 +1,55 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(
-  MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Image(
-          image: NetworkImage("https://www.pngkit.com/png/detail/344-3448185_diamantes-rosa-png-diamante-pink-png.png"),
-        ),
-      ),
-      appBar: AppBar(
-        backgroundColor: const Color(0xFFFF1744),
-        title: Text("I'm rich"),
-      ),
-    ),
-      debugShowCheckedModeBanner: false
-  )
-);
+void main() => runApp(XylophoneApp());
 
+class XylophoneApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Audio()
+      ),
+    );
+  }
+}
+class Audio extends StatefulWidget {
+  @override
+  _AudioState createState() => _AudioState();
+}
+
+class _AudioState extends State<Audio> {
+  static AudioCache player = AudioCache();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          criaBotao(Colors.red, 1),
+          criaBotao(Colors.orange, 2),
+          criaBotao(Colors.yellow, 3),
+          criaBotao(Colors.green, 4),
+          criaBotao(Colors.teal, 5),
+          criaBotao(Colors.blue, 6),
+          criaBotao(Colors.purple, 7),
+        ],
+      ),
+    );
+  }
+
+  Expanded criaBotao(Color cor, int botao){
+    return Expanded(
+      child: FlatButton(
+        color: cor,
+        onPressed:(){
+          player.play('note$botao.wav');
+        } 
+      ),
+    );
+  }
+}
